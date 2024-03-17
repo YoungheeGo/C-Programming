e8d1c8f51bb39ef47cb64205e294d28dd46f04b8
YoungheeGo <163238587+YoungheeGo@users.noreply.github.com>
2024-03-17 17:02:35 +0900

    Add files via upload

---
+++ 1강/1강/main.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d %d %d \n", a, b, c);
+	system("pause");
+	return 0;
+}
+
